fe23a42c4b9c1c67eca130ed40312f6b0d75e675
swhn <saiwaihlyanhtun@gmail.com>
2023-03-27 15:26:11 +0630

    test

--- chat.js
+++ chat.js
@@ -9,48 +9,106 @@ var firebaseConfig = {
   appId: "1:1034963106910:web:923d259f21adc2a53988cf"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 
-// Get a reference to the Firebase Realtime Database
-var database = firebase.database();
+var currentUser = null;
+var fileInput = document.getElementById("file-input");
 
-// Get a reference to the message list element
-var messageList = document.getElementById("message-list");
-
-// Listen for new messages in the Firebase database
-database.ref("messages").on("child_added", function(snapshot) {
-  var message = snapshot.val();
-  var li = document.createElement("li");
-  li.textContent = message.text;
-  messageList.appendChild(li);
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    currentUser = user;
+    document.getElementById("user-name").innerText = currentUser.displayName;
+    loadMessages();
+  } else {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
 });
 
-// Get a reference to the message input field and form
-var messageInput = document.getElementById("message-input");
-var messageForm = document.getElementById("message-form");
+function loadMessages() {
+  firebase
+    .database()
+    .ref("messages")
+    .on("child_added", function (snapshot) {
+      var data = snapshot.val();
+      displayMessage(snapshot.key, data.name, data.text, data.imageUrl);
+    });
+}
 
-// Listen for new messages to be submitted
-messageForm.addEventListener("submit", function(event) {
-  // Prevent the form from submitting normally
-  event.preventDefault();
+function sendMessage() {
+  var messageInput = document.getElementById("message-input");
+  var image = fileInput.files[0];
 
-  // Get the message text and current timestamp
-  var text = messageInput.value;
-  var now = new Date();
-  var timestamp = now.toLocaleString();
+  if (image) {
+    var storageRef = firebase.storage().ref("images/" + image.name);
+    var uploadTask = storageRef.put(image);
 
-  // Save the message to the database
-  database.ref("messages").push({
-    text: text,
-    timestamp: timestamp
-  });
+    uploadTask.on(
+      "state_changed",
+      function (snapshot) {
+        // Track upload progress if necessary
+      },
+      function (error) {
+        console.log(error);
+      },
+      function () {
+        uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+          firebase
+            .database()
+            .ref("messages")
+            .push({
+              name: currentUser.displayName,
+              text: "",
+              imageUrl: downloadURL,
+            })
+            .catch(function (error) {
+              console.error("Error writing new message to Firebase Database", error);
+            });
+        });
+      }
+    );
+  } else {
+    firebase
+      .database()
+      .ref("messages")
+      .push({
+        name: currentUser.displayName,
+        text: messageInput.value,
+      })
+      .catch(function (error) {
+        console.error("Error writing new message to Firebase Database", error);
+      });
+  }
 
-  // Clear the input field
   messageInput.value = "";
-});
+  fileInput.value = "";
+}
 
-// Clear the message list when the page loads
-window.onload = function() {
-  messageList.innerHTML = "";
-};
+function displayMessage(key, name, text, imageUrl) {
+  var messageList = document.getElementById("message-list");
+  var messageElement = document.createElement("div");
+  messageElement.classList.add("message");
+
+  if (imageUrl) {
+    var imageElement = document.createElement("img");
+    imageElement.src = imageUrl;
+    messageElement.appendChild(imageElement);
+  }
+
+  var nameElement = document.createElement("span");
+  nameElement.innerText = name;
+  messageElement.appendChild(nameElement);
+
+  var textElement = document.createElement("span");
+  textElement.innerText = text;
+  messageElement.appendChild(textElement);
+
+  messageList.appendChild(messageElement);
+  messageList.scrollTop = messageList.scrollHeight;
+}
+
+var sendMessageButton = document.getElementById("send-message-button");
+sendMessageButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  sendMessage();
+});
